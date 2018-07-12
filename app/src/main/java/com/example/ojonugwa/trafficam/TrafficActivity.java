b54ba0b66a0cdf7622dbd9e609e65eb88c3aedfe
Ojonugwa <ojonugwajusticealikali@gmail.com>
2018-07-12 14:17:36 +0100

    Create sample ListView

--- app/src/main/java/com/example/ojonugwa/trafficam/TrafficActivity.java
+++ app/src/main/java/com/example/ojonugwa/trafficam/TrafficActivity.java
@@ -5,6 +5,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TrafficActivity extends AppCompatActivity {
 
@@ -12,6 +16,25 @@ public class TrafficActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_traffic);
+
+        ArrayList<String> feeds = new ArrayList<>();
+        feeds.add("one");
+        feeds.add("two");
+        feeds.add("three");
+        feeds.add("four");
+        feeds.add("five");
+        feeds.add("six");
+        feeds.add("seven");
+        feeds.add("eight");
+        feeds.add("nine");
+        feeds.add("ten");
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, feeds);
+
+        ListView listView = findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
 
         // Setup FAB to open EditorActivity
         FloatingActionButton fab = findViewById(R.id.fab);
