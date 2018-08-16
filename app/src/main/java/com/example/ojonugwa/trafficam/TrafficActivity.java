6501a05f777e023d129b294ee0e9a9892e5615fe
Ojonugwa <ojonugwajusticealikali@gmail.com>
2018-08-16 14:51:55 +0100

    Complete RecyclerView draft

--- app/src/main/java/com/example/ojonugwa/trafficam/TrafficActivity.java
+++ app/src/main/java/com/example/ojonugwa/trafficam/TrafficActivity.java
@@ -4,9 +4,17 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 public class TrafficActivity extends AppCompatActivity {
+
+    // Completed (1) Create a private static final int called NUM_LIST_ITEMS and set it equal to 100
+    private static final int NUM_LIST_ITEMS = 100;
+
+    private TrafficAdapter mAdapter;
+    private RecyclerView mTrafficList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,23 @@ public class TrafficActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Completed (4) Use findViewById to store a reference to the RecyclerView in mNumbersList
+        mTrafficList = (RecyclerView) findViewById(R.id.rv_numbers);
+
+        // Completed (5) Create a LinearLayoutManager variable called layoutManager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        // Completed (6) Use setLayoutManager on mNumbersList with the LinearLayoutManager we created above
+        mTrafficList.setLayoutManager(layoutManager);
+
+        // Completed (7) Use setHasFixedSize(true) to designate that the contents of the RecyclerView won't change an item's size
+        mTrafficList.setHasFixedSize(true);
+
+        // Completed (8) Store a new GreenAdapter in mAdapter and pass it NUM_LIST_ITEMS
+        mAdapter = new TrafficAdapter(NUM_LIST_ITEMS);
+
+        // Completed (9) Set the GreenAdapter you created on mNumbersList
+        mTrafficList.setAdapter(mAdapter);
 
     }
 }
--- app/src/main/java/com/example/ojonugwa/trafficam/TrafficAdapter.java
+++ app/src/main/java/com/example/ojonugwa/trafficam/TrafficAdapter.java
@@ -1,28 +1,84 @@
 package com.example.ojonugwa.trafficam;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class TrafficAdapter {
+public class TrafficAdapter extends RecyclerView.Adapter<TrafficAdapter.TrafficViewHolder> {
 
+    private int mNumberItems;
+
+    /**
+     * Constructor for TrafficAdapter that accepts a number of items to display and the specification
+     * for the ListItemClickListener.
+     *
+     * @param numberOfItems Number of items to display in list
+     */
+    public TrafficAdapter(int numberOfItems) {
+        mNumberItems = numberOfItems;
+    }
+
+    @NonNull
+    @Override
+    public TrafficViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.traffic_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        TrafficViewHolder viewHolder = new TrafficViewHolder(view);
+
+        return viewHolder;
+    }
+
+
+    /**
+     * OnBindViewHolder is called by the RecyclerView to display the data at the specified
+     * position. In this method, we update the contents of the ViewHolder to display the correct
+     * indices in the list for this particular position, using the "position" argument that is conveniently
+     * passed into us.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(TrafficViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    /**
+     * This method simply returns the number of items to display. It is used behind the scenes
+     * to help layout our Views and for animations.
+     *
+     * @return The number of items available in our forecast
+     */
+    @Override
+    public int getItemCount() {
+        return mNumberItems;
+    }
 
     /**
      * Cache of the children views for a list item.
      */
     class TrafficViewHolder extends RecyclerView.ViewHolder {
 
-        TextView listItemNumberView;
+        TextView listItemTrafficView;
 
         public TrafficViewHolder(View itemView) {
             super(itemView);
 
-            listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+            listItemTrafficView = itemView.findViewById(R.id.tv_item_number);
         }
 
         void bind(int listIndex) {
-            listItemNumberView.setText(String.valueOf(listIndex));
+            listItemTrafficView.setText(String.valueOf(listIndex));
         }
     }
 }
